d3ebc09741ec4b4fc135258649524fae88805b1d
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-11-08 02:27:47 -0800

    Author Details Complete

--- author_mod.py
+++ author_mod.py
@@ -85,6 +85,33 @@ def add_author():
     finally:
         cursor.close()
         connection.close()
+
+def view_author_details():
+    clr()
+    hr(50)
+    try:
+        connection = connection_to_db()
+        cursor = connection.cursor()
+        name = input("Enter Author Name: ")
+
+        query = '''
+                SELECT * FROM authors
+                WHERE name = %s
+                '''
+        cursor.execute(query, (name,))
+        columns = cursor.column_names
+
+        print("Author Details:")
+
+        for row in cursor.fetchall():
+            hr(50)
+            row_data = dict(zip(columns, row))
+            print(f"ID: {row_data['id']}\nName: {row_data['name']}\nBio: {row_data['biography']}")
+        hr(50)
+
+    finally:
+        cursor.close()
+        connection.close()
 # From here down needs fixing 
 def display_authors():
     clr()
@@ -97,37 +124,18 @@ def display_authors():
         cursor.execute(query)
         columns = cursor.column_names
 
-        print("All Authors:\n")
+        print("All Authors:")
 
         for row in cursor.fetchall():
             hr(50)
             row_data = dict(zip(columns, row))
-            print(f"ID: {row_data['id']}\nName: {row_data['name']}\nBio: {row_data['biography']}")
-        hr(50)
-        
+            print(f"ID: {row_data['id']}\nName: {row_data['name']}")
+            hr(50)
+
     finally:
         cursor.close()
         connection.close()
 
-def view_author_details():
-    clr()
-    hr(50)
-    author_name = input("Enter Author's Name: ")
-
-    author_to_view = None
-    for author in Author.authors:
-        if author.get_author_name() == author_name:
-            author_to_view = author
-            break
-
-    if author_to_view:
-        clr()
-        hr(50)
-        print(f"Name: {author_to_view.get_author_name()}")
-        print(f"Biography: {author_to_view.get_biography()}")
-        hr(50)
-    else:
-        print(f"Author: {author_name} not found.")
 
 def author_menu():
     clr()
@@ -148,7 +156,7 @@ def author_menu():
             if author_operation == 1:
                 add_author()
             elif author_operation == 2:
-                pass
+                view_author_details()
             elif author_operation == 3:
                 display_authors()
             elif author_operation == 4:
